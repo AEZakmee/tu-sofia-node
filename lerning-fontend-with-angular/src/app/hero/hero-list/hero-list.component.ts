ac9909393b48171b464d112cb4e1027d4d7511c0
Kristiyan Tsankov <ktsankov@appolica.com>
2022-12-01 10:36:06 +0200

    Ex 3 changes

--- lerning-fontend-with-angular/src/app/hero/hero-list/hero-list.component.ts
+++ lerning-fontend-with-angular/src/app/hero/hero-list/hero-list.component.ts
@@ -9,13 +9,33 @@ import { HeroService } from '../services/hero.service';
 })
 export class HeroListComponent implements OnInit {
   public heroes: any;
+  public heroDialog: boolean = false;
+  public hero: Hero = {} as Hero;
 
   constructor(private heroService: HeroService) {}
 
   ngOnInit() {
     this.heroService.getAllHeroes().subscribe({
-      next: (heroes) => {
-        this.heroes = heroes;
+      next: (heroes : any) => {
+        let parsed = heroes.map((hero: any) => {
+            return {...hero, powers: hero.powers.map((power: any) => power.name).join(", ")};
+        });
+        this.heroes = parsed;
+      },
+      error: (err) => {},
+    });
+  }
+
+  showDialog() {
+    this.heroDialog = true;
+  }
+  
+  createHero() {
+    this.heroService.createHero(this.hero).subscribe({
+      next: () => {
+        this.heroDialog = false;
+        this.hero = {} as Hero;
+        this.ngOnInit();
       },
       error: (err) => {},
     });
